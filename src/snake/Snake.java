17eb7385ba6920878e4116d5c37934aaf02f329e
Mophe <87334468+Mopheshi@users.noreply.github.com>
2022-05-13 16:08:46 +0100

    Add files via upload

---
+++ src/snake/Snake.java
@@ -0,0 +1,31 @@
+package snake;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Ndachimya Edward
+ */
+public class Snake extends JFrame {
+
+    public Snake() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+        setResizable(false);
+        pack();
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
